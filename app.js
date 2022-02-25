abb4c5acb1295699ead84fb54dc7f2ba4c7f2ece
Vignesh <vigneshsweg123@gmail.com>
2022-02-25 21:31:40 +0530

    New Secrets Addition

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -88,13 +89,53 @@ app.get("/register", function (req, res) {
 });
 
 app.get("/secrets", function (req, res) {
+    User.find({ "secret": { $ne: null } }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUser) {
+                res.render("secrets", { userSecrets: foundUser });
+            }
+            else {
+                res.render("secrets", { userSecrets: [] });
+            }
+
+        }
+    });
+    // if (req.isAuthenticated()) {
+    //     res.render("secrets");
+    // }
+    // else {
+    //     res.redirect("/login");
+    // }
+});
+
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     }
     else {
         res.redirect("/login");
     }
 });
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user._id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+})
 
 app.get("/logout", function (req, res) {
     req.logout();
@@ -109,7 +150,7 @@ app.post("/register", function (req, res) {
             res.redirect("/register");
         }
         else {
-            passport.authenticate("local")(req, response, function () {
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/secrets");
             })
         }
